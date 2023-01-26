fc95987406994929a3b766389af154e9c765afb8
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-01-26 16:25:27 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-function defaultTask(cb) {
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { src, dest } = require('gulp');
+
+function defaultTask(cb)
+{
     // place code for your default task here
     cb();
 }
 
-exports.default = defaultTask
+function copy()
+{
+    return src(
+        [
+            '**/*',
+            '.gitignore',
+            '.eslintrc.json',
+            '!node_modules/**',
+            '!package-lock.json',
+            '!gulpfile.js',
+        ],
+        {
+            ignore: [
+            ],
+        })
+        .pipe(dest('publish/'));
+}
+
+
+exports.default = defaultTask;
+exports.copy = copy;
