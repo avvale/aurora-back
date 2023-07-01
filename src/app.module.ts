babb61ef2af6cb0c5def4fa14cd981a7b7ab098a
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-01 18:01:09 +0200

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
-import { CoreModule } from './@aurora/core.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// modules
+import { ScheduleModule } from '@nestjs/schedule';
+import { CoreModule } from './@aurora/core.module';
 import { AuditingModule } from '@api/auditing/auditing.module';
 import { OAuthModule } from '@api/o-auth/o-auth.module';
 import { IamModule } from '@api/iam/iam.module';
