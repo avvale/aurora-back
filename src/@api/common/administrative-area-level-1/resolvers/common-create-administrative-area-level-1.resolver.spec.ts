a19fc2b044c0fe69ee51d5295e726eff6ad2507a
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-08-08 19:21:15 +0200

    refactor administrative area level 1

--- src/@api/common/administrative-area-level-1/resolvers/common-create-administrative-area-level-1.resolver.spec.ts
+++ src/@api/common/administrative-area-level-1/resolvers/common-create-administrative-area-level-1.resolver.spec.ts
@@ -2,7 +2,6 @@
 import { CommonCreateAdministrativeAreaLevel1Handler, CommonCreateAdministrativeAreaLevel1Resolver } from '@api/common/administrative-area-level-1';
 import { CommonCreateAdministrativeAreaLevel1Input } from '@api/graphql';
 import { commonMockAdministrativeAreaLevel1Data } from '@app/common/administrative-area-level-1';
-import { ICommandBus, IQueryBus } from '@aurorajs.dev/core';
 import { Test, TestingModule } from '@nestjs/testing';
 
 describe('CommonCreateAdministrativeAreaLevel1Resolver', () =>
