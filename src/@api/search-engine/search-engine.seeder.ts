4dc96249c5c70abb610b9a3e907d7182dbe5e205
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-08-10 19:58:01 +0200

    minor fix

--- src/@api/search-engine/search-engine.seeder.ts
+++ src/@api/search-engine/search-engine.seeder.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ICommandBus, IQueryBus } from '@aurorajs.dev/core';
-// import { CreatePermissionsCommand } from '@app/iam/permission/application/create/create-permissions.command';
-// import { CreateBoundedContextsCommand } from '@app/iam/bounded-context/application/create/create-bounded-contexts.command';
+// import { IamCreatePermissionsCommand } from '@app/iam/permission';
+// import { IamCreateBoundedContextsCommand } from '@app/iam/bounded-context';
 import { boundedContexts, permissions } from '@app/search-engine/search-engine.seed';
 
 @Injectable()
@@ -14,8 +14,8 @@ export class SearchEngineSeeder
 
     async main(): Promise<boolean>
     {
-        // await this.commandBus.dispatch(new CreateBoundedContextsCommand(boundedContexts, { timezone: process.env.TZ }));
-        // await this.commandBus.dispatch(new CreatePermissionsCommand(permissions, { timezone: process.env.TZ }));
+        // await this.commandBus.dispatch(new IamCreateBoundedContextsCommand(boundedContexts, { timezone: process.env.TZ }));
+        // await this.commandBus.dispatch(new IamCreatePermissionsCommand(permissions, { timezone: process.env.TZ }));
 
         return true;
     }
