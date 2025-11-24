73c29e696ef0c702ed43f18513228782c41c7e39
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-11-24 20:36:15 +0100

    feat(support): integrate storage account file manager for screen recording uploads

--- src/@api/support/issue/handlers/support-create-issue.handler.ts
+++ src/@api/support/issue/handlers/support-create-issue.handler.ts
@@ -1,4 +1,5 @@
 import { SupportCreateIssueInput, SupportIssue } from '@api/graphql';
+import { StorageAccountFileManagerService } from '@api/storage-account/file-manager';
 import {
     CLICKUP_TASK_PLATFORM_API_KEY,
     CLICKUP_TASK_PLATFORM_LIST_ID,
@@ -8,17 +9,20 @@ import { SupportCreateIssueDto, SupportIssueDto } from '@api/support/issue';
 import {
     SupportCreateIssueCommand,
     SupportFindIssueByIdQuery,
+    SupportUpdateIssueByIdCommand,
 } from '@app/support/issue';
 import { AuditingMeta, ICommandBus, IQueryBus } from '@aurorajs.dev/core';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class SupportCreateIssueHandler {
     constructor(
         private readonly commandBus: ICommandBus,
         private readonly queryBus: IQueryBus,
+        private readonly storageAccountFileManagerService: StorageAccountFileManagerService,
         private readonly clickupService: ClickupService,
         @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     ) {}
@@ -37,6 +41,23 @@ export class SupportCreateIssueHandler {
             }),
         );
 
+        if (payload.screenRecording) {
+            void this.storageAccountFileManagerService
+                .uploadFile({
+                    ...payload.screenRecording,
+                    name: payload.screenRecording.file.name,
+                })
+                .then(
+                    (screenRecordingUploaded) =>
+                        void this.commandBus.dispatch(
+                            new SupportUpdateIssueByIdCommand({
+                                id: payload.id,
+                                screenRecording: screenRecordingUploaded,
+                            }),
+                        ),
+                );
+        }
+
         try {
             const clickupTaskPlatformApiKey =
                 await this.cacheManager.get<string>(
@@ -47,25 +68,17 @@ export class SupportCreateIssueHandler {
                 await this.cacheManager.get<string>(
                     CLICKUP_TASK_PLATFORM_LIST_ID,
                 );
-            /*  const res = await lastValueFrom(
-                this.clickupService.getFolders('90154074844', {
-                    authorization:
-                        'pk_6857068_25VQHBFU16YYHZ5OBYITIRYQPV46GSTZ',
-                }),
-            );
 
-            console.log('ClickUp folders response:', res); */
-
-            /* await lastValueFrom(
+            void lastValueFrom(
                 this.clickupService.createTask(
-                    '901516935164',
+                    clickupTaskPlatformListId,
                     {
                         name: payload.subject,
                         description: payload.description,
                     },
-                    { authorization: supportTaskPlatformApiKey },
+                    { authorization: clickupTaskPlatformApiKey },
                 ),
-            ); */
+            );
         } catch (error) {
             console.error('Error creating ClickUp task:', error);
         }
