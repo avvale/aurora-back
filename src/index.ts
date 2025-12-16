63fda9c0b67354eeddb298616882489988e30bf9
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-12-16 01:13:51 +0100

    feat: add support issue exports to index file for improved module accessibility

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { SupportIssue } from './@app/support/issue/domain/support-issue.aggregate';
+export { SupportIssueModel } from './@app/support/issue';
+export { SupportIssueResponse } from './@app/support/issue/domain/support-issue.response';
+export { SupportIssueMapper } from './@app/support/issue/domain/support-issue.mapper';
+export { supportMockIssueData } from './@app/support/issue/infrastructure/mock/support-mock-issue.data';
+export { SupportModule } from './@api/support/support.module';
+export { SupportIssueDto } from './@api/support/issue/dto/support-issue.dto';
